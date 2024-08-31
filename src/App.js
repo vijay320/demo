368bf651089de77e4b74989ff336b7ec1b25a231
Vijay Patel <vk86736@gmail.com>
2024-09-01 04:27:11 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -5,16 +5,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Vijay Patel</h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+       
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
+          >
+          <h2>Application deploy by Sushil singh</h2>
           Learn React
         </a>
       </header>
